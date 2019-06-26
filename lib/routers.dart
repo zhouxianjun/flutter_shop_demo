3d17704943a907640e9ceac80826765346f54a3c
Alone <gary@cn-face.com>
2019-06-26 16:05:43 +0800

    路由参数编码/解码

--- lib/routers.dart
+++ lib/routers.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:fluro/fluro.dart';
 import 'views/login.dart';
 import 'views/home/index.dart';
@@ -21,5 +23,18 @@ class Routers {
     router.define(C1, handler: Handler(handlerFunc: (context, params) => Counter1()));
     router.define(C2, handler: Handler(handlerFunc: (context, params) => Counter2()));
     Routers.router = router;
+  }
+
+  static String encode(String value) {
+    StringBuffer sb = StringBuffer();
+    List<int> encoded = Utf8Encoder().convert(value);
+    encoded.forEach((val) => sb.write('$val,'));
+    return sb.toString().substring(0, sb.length - 1).toString();
+  }
+
+  static String decode(String value) {
+    List<String> decoded = value.split('[').last.split(']').first.split(',');
+    List<int> list = decoded.map((s) => int.parse(s.trim()));
+    return Utf8Decoder().convert(list);
   }
 }
